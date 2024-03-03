acab90098be8b831257722ba38e3c355e0e19bed
hnoga-n <hoanghuynguyen711@gmail.com>
2024-03-03 11:29:36 +0700

    refactor: change all file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        // Simple 16 column grid
-        '16': 'repeat(16, minmax(0, 1fr))',
-
-        // Complex site-specific column configuration
-        'footer': '200px minmax(900px, 1fr) 100px',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
